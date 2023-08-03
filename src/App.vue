db4a268ff25c736ad1c87345b7547d41c011703f
rivalcoba <ivan.rivalcoba@gmail.com>
2023-08-03 11:41:59 -0500

    🛒 Modificadores en las propiedades

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ const newItem = ref('');
 
 <template>
   <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <input type="text" placeholder="Add Item" v-model="newItem">
+  <input type="text" placeholder="Add Item" v-model.trim="newItem">
   {{ newItem }}
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">
