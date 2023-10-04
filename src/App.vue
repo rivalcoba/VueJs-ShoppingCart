71fa81e19627085cbba583ea541a059f018570a3
rivalcoba <ivan.rivalcoba@gmail.com>
2023-10-04 12:28:33 -0500

    ⭐ Feature: Eventos de Usuario

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,19 @@ const newItemHighPriority = ref(false);
 
 <template>
   <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
-  <!-- Check Boxes -->
-  <label>
-    <input v-model="newItemHighPriority" 
-    type="checkbox">
-    Alta Prioridad
-  </label>
-  {{ newItemHighPriority ? "🔥" : "🧊" }}
+  <div class="add-item form">
+    <!-- Input de Nuevo Articulo -->
+    <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
+    <!-- Check Boxes -->
+    <label>
+      <input v-model="newItemHighPriority" 
+      type="checkbox">
+      Alta Prioridad
+    </label>
+    {{ newItemHighPriority ? "🔥" : "🧊" }}
+    <!-- Boton de UI -->
+    <button v-on:click="items.push({id: items.length + 1, label: newItem}) " class="btn btn-primary">Salvar Articulo</button>
+  </div>
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">
       🔹 {{ label }}
