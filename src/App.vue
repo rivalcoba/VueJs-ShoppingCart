d86565d74d7489462965230eb8a74c7b2dcacbcb
rivalcoba <ivan.rivalcoba@gmail.com>
2024-03-05 09:43:44 -0600

    ⭐ FEAT: App.vue Add destructuring to list

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ const items = ref([
 <template>
   <h1> <i :class="shoppingIcon">local_mall</i> {{ header }}</h1>
   <ul>
-    <li v-for="item in items" v-bind:key="item.id">⭐ {{ item.label }}</li>
+    <li v-for="{id, label} in items" v-bind:key="id">⭐ {{ label }}</li>
   </ul>
 </template>
 
