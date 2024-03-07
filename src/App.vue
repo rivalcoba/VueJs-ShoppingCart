3efa6ce713092ab91e15e0ddc9b6341f0784556f
rivalcoba <ivan.rivalcoba@gmail.com>
2024-03-07 09:23:46 -0600

    ⭐ FEAT: Checkboxes Bind checkboxes to newItemPriority

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,16 @@ const items = ref([
   { id: 4, label: 'Huevos' }
 ]);
 const newItem = ref('');
+const newItemPriority = ref('low')
 </script>
 
 <template>
   <h1> <i :class="shoppingIcon">local_mall</i> {{ header }}</h1>
   <input v-model="newItem" type="text" placeholder="Agregar articulo">
+  <!-- Radio Buttons -->
+  <label><input type="radio" v-model="newItemPriority" value="low">Baja</label>
+  <label><input type="radio" v-model="newItemPriority" value="high">Alta</label>
+  {{ newItemPriority === 'low'?'🧊':'🔥' }}
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">⭐ {{ label }}</li>
   </ul>
