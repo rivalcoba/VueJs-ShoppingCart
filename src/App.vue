d75d02bfa7c7eab12c7378e004760b4d18d90801
rivalcoba <ivan.rivalcoba@gmail.com>
2024-03-15 08:00:13 -0600

    ⭐ FEAT: v-else

--- src/App.vue
+++ src/App.vue
@@ -9,38 +9,67 @@ const shoppingIcon = ref('material-icons shopping-cart-icon');
 // Creando una referencia reactiva
 // para alamacenar el valor de la lista
 const items = ref([
-  { id: 0, label: 'Leche' },
-  { id: 1, label: 'Arroz' },
-  { id: 2, label: 'Carne' },
-  { id: 3, label: 'Pan' },
-  { id: 4, label: 'Huevos' }
+  // { id: 0, label: 'Leche' },
+  // { id: 1, label: 'Arroz' },
+  // { id: 2, label: 'Carne' },
+  // { id: 3, label: 'Pan' },
+  // { id: 4, label: 'Huevos' }
 ]);
 const newItem = ref('');
 const newItemHighPriority = ref(false)
+// Metodos
+const saveItems = () => {
+  // Agrega un nuevo elemento a la lista
+  // proveniente de la caja de texto
+  items.value.push({ id: items.value.length, label: newItem.value })
+  // Borramos el contenido de la caja de texto
+  newItem.value = "";
+};
+const doEdit = (edit) => {
+  editing.value = edit;
+  newItem.value = "";
+}
+const editing = ref(false);
 </script>
 
 <template>
-  <h1> <i :class="shoppingIcon">local_mall</i> {{ header }}</h1>
-  <form 
-    v-on:submit.prevent="items.push({ id: items.length, label: newItem})" 
-    class="add-item form">
-    <input 
-      v-model="newItem" type="text" 
-      placeholder="Agregar articulo">
+  <!-- Header -->
+  <div class="header">
+    <h1>
+      <i :class="shoppingIcon">local_mall</i> {{ header }}
+    </h1>
+    <button 
+      v-on:click="doEdit(false)"
+      v-if="editing" 
+      class="btn">
+      Cancelar
+    </button>
+    <button 
+      v-else
+      v-on:click="doEdit(true)" 
+      class="btn btn-primary">
+      Agregar Articulo
+    </button>
+  </div>
+  <!-- Formulario -->
+  <form v-if="editing" v-on:submit.prevent="saveItems" class="add-item form">
+    <input v-model="newItem" type="text" placeholder="Agregar articulo">
     <!-- Checkbox -->
     <label>
       <input type="checkbox" v-model="newItemHighPriority">
       Alta Prioridad
     </label>
     <!-- Boton -->
-    <button 
-      class="btn btn-primary">
+    <button class="btn btn-primary">
       Agregar Articulo
     </button>
   </form>
+  <!-- Entrega de lista -->
   <ul>
     <li v-for="{ id, label } in items" v-bind:key="id">⭐ {{ label }}</li>
   </ul>
+  <!-- Mensaje condicional -->
+  <p v-if="items.length === 0">🥀 No hay elementos en la lista 🥀</p>
 </template>
 
 <style scoped>
