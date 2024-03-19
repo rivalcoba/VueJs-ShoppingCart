8f1b3ba69543a8c65043e4fc347abfbc594c8ed4
rivalcoba <ivan.rivalcoba@gmail.com>
2024-03-19 09:50:04 -0600

    ⭐ FEAT: Clase enlazada

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@ const shoppingIcon = ref('material-icons shopping-cart-icon');
 // Creando una referencia reactiva
 // para alamacenar el valor de la lista
 const items = ref([
-  // { id: 0, label: 'Leche' },
-  // { id: 1, label: 'Arroz' },
-  // { id: 2, label: 'Carne' },
-  // { id: 3, label: 'Pan' },
-  // { id: 4, label: 'Huevos' }
+  { id: 0, label: 'Leche', purchased: false, highPriority: true },
+  { id: 1, label: 'Arroz', purchased: false, highPriority: false },
+  { id: 2, label: 'Carne', purchased: true, highPriority: false },
+  { id: 3, label: 'Pan', purchased: false, highPriority: true },
+  { id: 4, label: 'Huevos', purchased: true, highPriority: true }
 ]);
 const newItem = ref('');
 const newItemHighPriority = ref(false)
@@ -66,7 +66,12 @@ const editing = ref(false);
   </form>
   <!-- Entrega de lista -->
   <ul>
-    <li v-for="{ id, label } in items" v-bind:key="id">⭐ {{ label }}</li>
+    <li 
+      v-for="{ id, label, purchased, highPriority } in items"
+      :class="{priority: highPriority, strikeout:purchased}"
+      v-bind:key="id">
+      ⭐ {{ label }}
+    </li>
   </ul>
   <!-- Mensaje condicional -->
   <p v-if="items.length === 0">🥀 No hay elementos en la lista 🥀</p>
